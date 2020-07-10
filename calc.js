1b6395ad84632b10b89383598127dfbcd17894e5
James <jamesbaker425@gmail.com>
2020-07-09 20:27:31 -0400

    Update calc.js
    Added keyboard functionality.

--- calc.js
+++ calc.js
@@ -5,11 +5,29 @@
     jamesbaker425@gmail.com
 */
 
-//Whenever the user clicks a number button, this function will
+//Whenever the user clicks a number button, it is sent to be input to the display
+function inputClick(e) {
+    let inp = e.target.getAttribute("value");
+    input(inp);
+}
+
+//Whenever the keyboard is used, this function directs valid
+//input to the proper function
+function inputKey(e) {
+    if (e.key >= "0" && e.key <= "9") { input(e.key); }
+    else if (e.key == "Backspace") { backspace(); }
+    else if (e.key == "+") { operator("add"); }
+    else if (e.key == "-") { operator("sub"); }
+    else if (e.key == "*") { operator("mul"); }
+    else if (e.key == "/") { operator("div"); }
+    else if (e.key == ".") { point(); }
+    else if (e.key == "=" || e.key == "Enter") { operate(); }
+}
+
+//For any numbers being clicked or typed, this function will
 //add that number as the next digit they are typing, whatever the
 //display string is gets converted into a number from there
-function input(e) {
-    let inp = e.target.getAttribute("value");
+function input(inp) {
     //Gets rid of 0 if it's the only thing displayed,
     //otherwise, it adds to the string
     if (freshInput) { 
@@ -24,6 +42,8 @@ function input(e) {
     num2 = Number(dis);
 }
 
+//This function will slice off the last character of the display
+//and change the numbers in the calculator to represent what is displayed
 function backspace() {
     if (freshInput && dis != "") {
         clear();
@@ -32,7 +52,6 @@ function backspace() {
     freshInput = dis == "";
     document.getElementById("dis").innerHTML = dis;
     num2 = Number(dis);
-    console.log("dis: ", dis);
 }
 
 //Whenever the "." button is pressed, we add that to the display string.
@@ -54,10 +73,15 @@ function point() {
 }
 
 //Gets called whenever a button for an operator (+,-,*,/)
-//gets pressed. Tells the program which operator to use in lastOp.
-function operator(e) {
+//gets pressed. 
+function operatorClick(e) {
     var op = e.target.getAttribute("id")
+    operator(op);
+}
 
+//Tells the program which operator to use as lastOp. Also responsible
+//for displaying answer if chaining operators
+function operator(op) {
     //If chaining operations together, we need to keep the result
     //of the preceeding operation.
     if (!isFirstOp) {
@@ -74,6 +98,7 @@ function operator(e) {
     isFirstOp = false;
     freshInput = true;
     pointUsed = false;
+    document.getElementById("LastOp").innerHTML = ops[op];
 }
 
 //Gets called when user clicks the "=" button. Performs the
@@ -126,8 +151,8 @@ function clear() {
     isFirstOp = true;
     freshInput = true;
     pointUsed = false;
-
     document.getElementById("dis").innerHTML = dis;
+    document.getElementById("LastOp").innerHTML = ops[lastOp];
 }
 
 //Helper function to round long floats into a specified amount of decimal places 
@@ -138,12 +163,15 @@ function roundFloat(num, places) {
 //-------------------------------------------------//
 //---Where the script starts; main() if you will.---
 //-------------------------------------------------//
+const ops = {"add": "+", "sub": "-", "mul": "&times;", "div": "&divide;", "": ""};
 
 //Set up event listeners for numbers
 nums = document.getElementsByClassName("num");
 for (let elem of nums) {
-    elem.addEventListener("click", input);
+    elem.addEventListener("click", inputClick);
 }
+
+document.addEventListener("keydown", inputKey);
 
 var num1 = 0;
 var num2 = 0;
@@ -154,10 +182,10 @@ var freshInput = true;
 var pointUsed = false;
 
 //Set up event listeners for operators and clear
-document.getElementById("add").addEventListener("click", operator);
-document.getElementById("sub").addEventListener("click", operator);
-document.getElementById("mul").addEventListener("click", operator);
-document.getElementById("div").addEventListener("click", operator);
+document.getElementById("add").addEventListener("click", operatorClick);
+document.getElementById("sub").addEventListener("click", operatorClick);
+document.getElementById("mul").addEventListener("click", operatorClick);
+document.getElementById("div").addEventListener("click", operatorClick);
 document.getElementById("eq").addEventListener("click", operate);
 document.getElementById("clear").addEventListener("click", clear);
 document.getElementById("pt").addEventListener("click", point);
